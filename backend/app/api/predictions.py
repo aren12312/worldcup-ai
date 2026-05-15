8d04a59656ed20537168459a0dac8fcbff553243
aren12312 <game1mail11@gmail.com>
2026-05-15 20:26:30 +0300

    Update predictions.py

--- backend/app/api/predictions.py
+++ backend/app/api/predictions.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
-from app.services.prediction_engine import generate_prediction
+
+from backend.app.services.prediction_engine import generate_prediction
 
 router = APIRouter()
 
 @router.get("/predict")
+
 async def predict(team1: str, team2: str):
 
-    return generate_prediction(team1, team2)
+    prediction = generate_prediction(team1, team2)
+
+    return prediction
