d3847453b18d876d5f178476304ec42ed3ae71f3
aren12312 <game1mail11@gmail.com>
2026-05-15 19:59:33 +0300

    Update api_football.py

--- backend/app/services/api_football.py
+++ backend/app/services/api_football.py
@@ -5,14 +5,34 @@ API_KEY = os.getenv("API_FOOTBALL_KEY")
 
 BASE_URL = "https://v3.football.api-sports.io"
 
-async def get_team_statistics(team_id: int):
-    headers = {"x-apisports-key": API_KEY}
+HEADERS = {
+    "x-apisports-key": API_KEY
+}
+
+async def get_live_matches():
 
     async with httpx.AsyncClient() as client:
+
+        response = await client.get(
+            f"{BASE_URL}/fixtures",
+            headers=HEADERS,
+            params={"live": "all"}
+        )
+
+    return response.json()
+
+async def get_team_statistics(team_id):
+
+    async with httpx.AsyncClient() as client:
+
         response = await client.get(
             f"{BASE_URL}/teams/statistics",
-            headers=headers,
-            params={"team": team_id}
+            headers=HEADERS,
+            params={
+                "team": team_id,
+                "league": 39,
+                "season": 2025
+            }
         )
 
     return response.json()
